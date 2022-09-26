04853be10217e8e574e8e856ef1937c19efb3e65
RenatoLucena <reh.lucena88@gmail.com>
2022-09-26 14:27:59 -0300

    adcionado os arquivos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
+import Axios from 'axios'
 
 function App() {
+  const [number, setNumber] = useState(0)
+  const [product, setProduct] = useState('')
+  const [productList, setProductList] = useState([])
+
+  const addProduct = () => {
+    Axios.post('http://localhost:3002/create', {
+      number: number,
+      product: product
+    }).then(() => {
+      alert('Novo Produto Adicionado')
+    })
+  }
+
+  const showProducts = () => {
+    Axios.get('http://localhost:3002/products').then(response => {
+      setProductList(response.data)
+    })
+  }
+
+  const deleteProduct = number => {
+    Axios.delete(`http://localhost:3002/delete/${number}`).then(response => {
+      alert('Produto Deletado')
+      setProductList(
+        productList.filter(val => {
+          return val.number !== number
+        })
+      )
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="produtos">
+        <label>Numero</label>
+        <input
+          type="number"
+          onChange={event => {
+            setNumber(event.target.value)
+          }}
+        />
+        <label>Produto</label>
+        <input
+          type="text"
+          onChange={event => {
+            setProduct(event.target.value)
+          }}
+        />
+        <button onClick={addProduct}>Adicionar Produto</button>
+      </div>
+      <div className="mostrarProdutos">
+        <button onClick={showProducts}>Mostrar Produtos</button>
+        {productList.map((val, key) => {
+          return (
+            <div className="onDisplay">
+              <h3>ID: {val.number}</h3>
+              <h3>Produto: {val.product}</h3>
+              <button
+                onClick={() => {
+                  deleteProduct(val.number)
+                }}
+              >
+                Remover Produto
+              </button>
+            </div>
+          )
+        })}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
